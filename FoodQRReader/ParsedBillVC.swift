ff251e295181d79c2fc20708a21f9bbf678ebb2d
LEONID NIFANTIJEV <leonif1@gmail.com>
2018-08-01 13:48:06 +0300

    Update ParsedBillVC.swift

--- FoodQRReader/ParsedBillVC.swift
+++ FoodQRReader/ParsedBillVC.swift
@@ -14,19 +14,13 @@ class ParsedBillVC: UIViewController, UITableViewDelegate, UITableViewDataSource
         
         billTableView.delegate = self
         billTableView.dataSource = self
-        
-
-        // Do any additional setup after loading the view.
     }
-    
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return 1
     }
-
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         return UITableViewCell()
     }
-    
 }
